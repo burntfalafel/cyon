539f259907bbed152272cc90767926aff46c3dad
burntfalafel <wadhwae@tcd.ie>
2020-12-30 17:28:49 +0000

    Binary operations in VM stack implemented

--- vm.c
+++ vm.c
@@ -20,6 +20,12 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
     #ifdef DEBUG_TRACE_EXECUTION
@@ -44,6 +50,10 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
       case OP_NEGATE:   push(-pop()); break;
       case OP_RETURN: {
         printValue(pop());
@@ -52,9 +62,9 @@ static InterpretResult run() {
       }
     }
   }
-
-#undef READ_BYTE
+#undef BINARY_OP
 #undef READ_CONSTANT
+#undef READ_BYTE
 }
 
 InterpretResult interpret(Chunk* chunk) {
