3656f1c3c78e84597e35d18de90c5f4bcd0eec5f
burntfalafel <wadhwae@tcd.ie>
2021-06-28 14:26:23 +0100

    added strings as an object type to the functionality of cyon. supports > concatenation > freeing of (string) objects - done using an intrusive (single-linked) list. a simple replacement to the garbage collector for now

--- vm.c
+++ vm.c
@@ -1,8 +1,11 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
 #include "debug.h"
+#include "object.h"
+#include "memory.h"
 #include "vm.h"
 #include "compiler.h"
 
@@ -13,10 +16,12 @@ static void resetStack() {
 }
 
 void initVM() {
-    resetStack();
+  resetStack();
+  vm.objects = NULL; // for GC
 }
 
 void freeVM() {
+  freeObjects(); // for GC
 }
 
 void push(Value value) {
@@ -35,6 +40,20 @@ static Value peek(int distance) {
 
 static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+  ObjString* b = AS_STRING(pop());
+  ObjString* a = AS_STRING(pop());
+
+  int length = a->length + b->length;
+  char* chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString* result = takeString(chars, length);
+  push(OBJ_VAL(result));
 }
 
 // varadic C function which copies the printf() format
@@ -99,7 +118,20 @@ static InterpretResult run() {
       }
       case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
       case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
-      case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+      case OP_ADD: {
+        if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+          concatenate();
+        } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+          double b = AS_NUMBER(pop());
+          double a = AS_NUMBER(pop());
+          push(NUMBER_VAL(a + b));
+        } else {
+          runtimeError(
+              "Operands must be two numbers or two strings.");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        break;
+      }
       case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
       case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
       case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
